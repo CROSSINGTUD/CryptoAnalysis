b30b69d1f45f139279a6c863b119da72f3abcb9d
Johannes Spaeth <johannes.spaeth@iem.fraunhofer.de>
2017-06-12 16:57:11 +0200

    Implementation of Zoo Downloader

--- AndroidCallGraph/src/main/java/main/Executor.java
+++ AndroidCallGraph/src/main/java/main/Executor.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
+import android.zoo.Downloader;
+
 public class Executor {
 	public static void main(String... args) {
 		String classpath = System.getProperty("java.class.path");
 		String javaHome = System.getProperty("java.home");
 		int i = 0;
-		File[] listFiles = new File("D:\\CROSSING\\android-apps").listFiles();
+		File[] listFiles = Downloader.DOWNLOAD_DIRECTORY.listFiles();
 		for (File file : listFiles) {
 			System.out.println(file);
 			if (file.getName().endsWith(".apk") || file.getName().endsWith(".APK")) {
@@ -30,7 +32,7 @@ public class Executor {
 					ex.printStackTrace();
 				}
 			}
-			break;
+//			break;
 ////			i++;
 //			if(i > 10)
 //				break;
