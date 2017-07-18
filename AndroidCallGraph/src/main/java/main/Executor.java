a96fa3d22b1435f5c0578e9a31b7b9e77d8424c9
Johannes Späth <joh.spaeth@googlemail.com>
2017-07-18 12:34:26 +0200

    Move file to timeout on execute

--- AndroidCallGraph/src/main/java/main/Executor.java
+++ AndroidCallGraph/src/main/java/main/Executor.java
@@ -3,6 +3,10 @@ package main;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.io.Files;
 
 import android.zoo.Downloader;
 
@@ -16,7 +20,7 @@ public class Executor {
 			System.out.println(file);
 			if (file.getName().endsWith(".apk") || file.getName().endsWith(".APK")) {
 				String[] command = new String[] { javaHome + File.separator + "bin" + File.separator + "java","-Xmx8g","-Xss16m", "-cp",
-						classpath, PerAPKAnalyzer.class.getName(), file.getAbsolutePath(), args[0]};
+					classpath, PerAPKAnalyzer.class.getName(), file.getAbsolutePath(), args[0]};
 				System.out.println("Running command: " + Arrays.toString(command));
 				try {
 					ProcessBuilder pb = new ProcessBuilder(command);
@@ -26,7 +30,21 @@ public class Executor {
 					pb.redirectOutput(new File("target/reports/"+file.getName() + "-out.txt"));
 					pb.redirectError(new File("target/reports/" +  file.getName() + "-err.txt"));
 					Process proc = pb.start();
-					proc.waitFor();
+
+					boolean finished = proc.waitFor(Integer.parseInt(args[1]), TimeUnit.MINUTES);
+					if (!finished) {
+						proc.destroy();
+						proc.waitFor(); // wait for the process to terminate
+					
+						String folder = file.getParent();
+						String analyzedFolder = folder+ File.separator +  "timedout";
+						File dir = new File(analyzedFolder);
+						if(!dir.exists()){
+							dir.mkdir();
+						}
+						File moveTo = new File(dir.getAbsolutePath()+File.separator+file.getName());
+						Files.move(file, moveTo);
+					}
 				} catch (IOException ex) {
 					System.err.println("Could not execute timeout command: " + ex.getMessage());
 					ex.printStackTrace();
