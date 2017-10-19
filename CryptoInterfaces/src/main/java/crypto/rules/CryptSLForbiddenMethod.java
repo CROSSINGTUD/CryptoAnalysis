d82d2b99529f01f0f7474ae17077e676f64120c3
Stefan Krueger <stefan.krueger@crossing.tu-darmstadt.de>
2017-10-19 10:58:00 +0200

    Alternative methods now stored in CrySLForbiddenMethod objects. #33

--- CryptoInterfaces/src/main/java/crypto/rules/CryptSLForbiddenMethod.java
+++ CryptoInterfaces/src/main/java/crypto/rules/CryptSLForbiddenMethod.java
@@ -1,27 +1,36 @@
 package crypto.rules;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CryptSLForbiddenMethod implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private CryptSLMethod meth;
 	private Boolean silent;// = false;
+	private List<CryptSLMethod> alternate;
 	
 	public CryptSLForbiddenMethod(CryptSLMethod method, Boolean silent) {
+		this(method, silent, new ArrayList<CryptSLMethod>());
+	}
+	
+	public CryptSLForbiddenMethod(CryptSLMethod method, Boolean silent, List<CryptSLMethod> alternatives) {
 		this.meth = method;
 		this.silent = silent;
+		this.alternate = alternatives;
+	}
+
+	public CryptSLMethod getMethod() {
+		return meth;
 	}
 	
 	public Boolean getSilent() {
 		return silent;
 	}
 	
-	public CryptSLMethod getMethod() {
-		return meth;
+	public List<CryptSLMethod> getAlternatives() {
+		return alternate;
 	}
 	
 	public String toString() {
