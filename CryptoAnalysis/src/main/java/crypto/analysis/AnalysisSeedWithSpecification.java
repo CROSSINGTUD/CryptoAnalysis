3f9e9cb6cbb7378bf9527777bd0e6c42b9c7b14e
Johannes Späth <joh.spaeth@googlemail.com>
2018-09-04 10:29:14 +0200

    Add missing import

--- CryptoAnalysis/src/main/java/crypto/analysis/AnalysisSeedWithSpecification.java
+++ CryptoAnalysis/src/main/java/crypto/analysis/AnalysisSeedWithSpecification.java
@@ -3,6 +3,7 @@ package crypto.analysis;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
