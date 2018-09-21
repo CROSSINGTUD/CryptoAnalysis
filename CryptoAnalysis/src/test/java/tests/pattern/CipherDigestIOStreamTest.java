6c55284a87483ab47aa4b54b565a2a21c32371c1
Enri Ozuni <enriozuni@hotmail.com>
2018-09-21 09:54:43 +0200

    changed the name of UsagePatternTestExtra.java to CipherDigestIOStreamTest.java

--- CryptoAnalysis/src/test/java/tests/pattern/CipherDigestIOStreamTest.java
+++ CryptoAnalysis/src/test/java/tests/pattern/CipherDigestIOStreamTest.java
@@ -23,7 +23,7 @@ import org.junit.Test;
 import test.UsagePatternTestingFramework;
 import test.assertions.Assertions;
 
-public class UsagePatternTestExtra extends UsagePatternTestingFramework{
+public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 
 	//Usage Pattern tests for DigestInputStream
 	@Test
