5ebb86af0f3b003480f50060aa3cbf0b851b6da7
Enri Ozuni <enriozuni@hotmail.com>
2018-09-25 13:26:51 +0200

    excluded CipherDigestIOStreamTest in the pom.xml so the build does not take so long in shippable

--- CryptoAnalysis/src/test/java/tests/pattern/CipherDigestIOStreamTest.java
+++ CryptoAnalysis/src/test/java/tests/pattern/CipherDigestIOStreamTest.java
@@ -54,17 +54,17 @@ public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 	  }
 	}
 
-//	@Test
-//	public void UsagePatternTest23b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  InputStream is = new FileInputStream(".\\resources\\dis.txt");
-//	  MessageDigest md = MessageDigest.getInstance("SHA-256");
-//	  Assertions.extValue(0);
-//	  DigestInputStream dis = new DigestInputStream(is, md);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  dis.read("input".getBytes(), 0, "input".getBytes().length);
-//	  Assertions.mustBeInAcceptingState(dis);
-//	}
+	@Test
+	public void UsagePatternTest23b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  InputStream is = new FileInputStream(".\\resources\\dis.txt");
+	  MessageDigest md = MessageDigest.getInstance("SHA-256");
+	  Assertions.extValue(0);
+	  DigestInputStream dis = new DigestInputStream(is, md);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  dis.read("input".getBytes(), 0, "input".getBytes().length);
+	  Assertions.mustBeInAcceptingState(dis);
+	}
 
 	@Test
 	public void UsagePatternTest23c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
@@ -93,17 +93,17 @@ public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 	  Assertions.mustNotBeInAcceptingState(dos);
 	}
 
-//	@Test
-//	public void UsagePatternTest24a() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  OutputStream os = new FileOutputStream(".\\resources\\dos.txt");
-//	  MessageDigest md = MessageDigest.getInstance("SHA-256");
-//	  Assertions.extValue(0);
-//	  DigestOutputStream dos = new DigestOutputStream(os, md);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  Assertions.mustNotBeInAcceptingState(dos);
-//	  dos.write(new String("Hello World").getBytes());
-//	}
+	@Test
+	public void UsagePatternTest24a() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  OutputStream os = new FileOutputStream(".\\resources\\dos.txt");
+	  MessageDigest md = MessageDigest.getInstance("SHA-256");
+	  Assertions.extValue(0);
+	  DigestOutputStream dos = new DigestOutputStream(os, md);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  Assertions.mustNotBeInAcceptingState(dos);
+	  dos.write(new String("Hello World").getBytes());
+	}
 
 	@Test
 	public void UsagePatternTest24b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
@@ -117,18 +117,18 @@ public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 	  Assertions.mustBeInAcceptingState(dos);
 	}
 
-//	@Test
-//	public void UsagePatternTest24c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  OutputStream os = new FileOutputStream(".\\resources\\dos.txt");
-//	  MessageDigest md = MessageDigest.getInstance("SHA-256");
-//	  Assertions.extValue(0);
-//	  DigestOutputStream dos = new DigestOutputStream(os, md);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  dos.write("message".getBytes(), 100, "message".getBytes().length);
-//	  Assertions.violatedConstraint(dos);
-////			Assertions.mustNotBeInAcceptingState(dos);
-//	}
+	@Test
+	public void UsagePatternTest24c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  OutputStream os = new FileOutputStream(".\\resources\\dos.txt");
+	  MessageDigest md = MessageDigest.getInstance("SHA-256");
+	  Assertions.extValue(0);
+	  DigestOutputStream dos = new DigestOutputStream(os, md);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  dos.write("message".getBytes(), 100, "message".getBytes().length);
+	  Assertions.violatedConstraint(dos);
+//			Assertions.mustNotBeInAcceptingState(dos);
+	}
 
 	//Usage Pattern for CipherOutputStream
 	@Test
@@ -177,78 +177,78 @@ public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 	  cos.close();
 	}
 
-//	@Test
-//	public void UsagePatternTest25b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-//	  Assertions.extValue(0);
-//	  keyGenerator.init(128);
-//	  Assertions.extValue(0);
-//	  SecretKey key = keyGenerator.generateKey();
-//	  Assertions.hasEnsuredPredicate(key);
-//	  Assertions.mustBeInAcceptingState(keyGenerator);
-//	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//	  Assertions.extValue(0);
-//	  cipher.init(Cipher.ENCRYPT_MODE, key);
-//	  Assertions.extValue(0);
-//
-//	  OutputStream os = new FileOutputStream(".\\resources\\cos.txt");
-//	  CipherOutputStream cos = new CipherOutputStream(os, cipher);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  cos.write("message".getBytes(), 0, "message".getBytes().length);
-//	  cos.close();
-//	  Assertions.mustBeInAcceptingState(cos);
-//	}
+	@Test
+	public void UsagePatternTest25b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	  Assertions.extValue(0);
+	  keyGenerator.init(128);
+	  Assertions.extValue(0);
+	  SecretKey key = keyGenerator.generateKey();
+	  Assertions.hasEnsuredPredicate(key);
+	  Assertions.mustBeInAcceptingState(keyGenerator);
+	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	  Assertions.extValue(0);
+	  cipher.init(Cipher.ENCRYPT_MODE, key);
+	  Assertions.extValue(0);
 
-//	@Test
-//	public void UsagePatternTest25c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-//	  Assertions.extValue(0);
-//	  keyGenerator.init(128);
-//	  Assertions.extValue(0);
-//	  SecretKey key = keyGenerator.generateKey();
-//	  Assertions.hasEnsuredPredicate(key);
-//	  Assertions.mustBeInAcceptingState(keyGenerator);
-//	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//	  Assertions.extValue(0);
-//	  cipher.init(Cipher.ENCRYPT_MODE, key);
-//	  Assertions.extValue(0);
-//
-//	  OutputStream os = new FileOutputStream(".\\resources\\cos.txt");
-//	  CipherOutputStream cos = new CipherOutputStream(os, cipher);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  cos.write("message".getBytes(), 100, "message".getBytes().length);
-////		Assertions.violatedConstraint(cos);
-//	  Assertions.mustNotBeInAcceptingState(cos);
-//	  cos.close();
-//	}
+	  OutputStream os = new FileOutputStream(".\\resources\\cos.txt");
+	  CipherOutputStream cos = new CipherOutputStream(os, cipher);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  cos.write("message".getBytes(), 0, "message".getBytes().length);
+	  cos.close();
+	  Assertions.mustBeInAcceptingState(cos);
+	}
+
+	@Test
+	public void UsagePatternTest25c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	  Assertions.extValue(0);
+	  keyGenerator.init(128);
+	  Assertions.extValue(0);
+	  SecretKey key = keyGenerator.generateKey();
+	  Assertions.hasEnsuredPredicate(key);
+	  Assertions.mustBeInAcceptingState(keyGenerator);
+	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	  Assertions.extValue(0);
+	  cipher.init(Cipher.ENCRYPT_MODE, key);
+	  Assertions.extValue(0);
+
+	  OutputStream os = new FileOutputStream(".\\resources\\cos.txt");
+	  CipherOutputStream cos = new CipherOutputStream(os, cipher);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  cos.write("message".getBytes(), 100, "message".getBytes().length);
+//		Assertions.violatedConstraint(cos);
+	  Assertions.mustNotBeInAcceptingState(cos);
+	  cos.close();
+	}
 
 	//Usage Pattern tests for CipherInputStream
-//	@Test
-//	public void UsagePatternTest26() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-//	  Assertions.extValue(0);
-//	  keyGenerator.init(128);
-//	  Assertions.extValue(0);
-//	  SecretKey key = keyGenerator.generateKey();
-//	  Assertions.hasEnsuredPredicate(key);
-//	  Assertions.mustBeInAcceptingState(keyGenerator);
-//	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//	  Assertions.extValue(0);
-//	  cipher.init(Cipher.DECRYPT_MODE, key);
-//	  Assertions.extValue(0);
-//
-//	  InputStream is = new FileInputStream(".\\resources\\cis.txt");
-//	  CipherInputStream cis = new CipherInputStream(is, cipher);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  while (cis.read() != -1) {
-//
-//	  }
-//	  cis.close();
-//	  Assertions.mustBeInAcceptingState(cis);
-//	}
+	@Test
+	public void UsagePatternTest26() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	  Assertions.extValue(0);
+	  keyGenerator.init(128);
+	  Assertions.extValue(0);
+	  SecretKey key = keyGenerator.generateKey();
+	  Assertions.hasEnsuredPredicate(key);
+	  Assertions.mustBeInAcceptingState(keyGenerator);
+	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	  Assertions.extValue(0);
+	  cipher.init(Cipher.DECRYPT_MODE, key);
+	  Assertions.extValue(0);
+
+	  InputStream is = new FileInputStream(".\\resources\\cis.txt");
+	  CipherInputStream cis = new CipherInputStream(is, cipher);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  while (cis.read() != -1) {
+
+	  }
+	  cis.close();
+	  Assertions.mustBeInAcceptingState(cis);
+	}
 
 	@Test
 	public void UsagePatternTest26a() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
@@ -273,30 +273,30 @@ public class CipherDigestIOStreamTest extends UsagePatternTestingFramework{
 	  Assertions.mustBeInAcceptingState(cis);
 	}
 
-//	@Test
-//	public void UsagePatternTest26b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
-//	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-//	  Assertions.extValue(0);
-//	  keyGenerator.init(128);
-//	  Assertions.extValue(0);
-//	  SecretKey key = keyGenerator.generateKey();
-//	  Assertions.hasEnsuredPredicate(key);
-//	  Assertions.mustBeInAcceptingState(keyGenerator);
-//	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-//	  Assertions.extValue(0);
-//	  cipher.init(Cipher.DECRYPT_MODE, key);
-//	  Assertions.extValue(0);
-//
-//	  InputStream is = new FileInputStream(".\\resources\\cis.txt");
-//	  CipherInputStream cis = new CipherInputStream(is, cipher);
-//	  Assertions.extValue(0);
-//	  Assertions.extValue(1);
-//	  while (cis.read() != -1) {
-//
-//	  }
-//	  Assertions.mustNotBeInAcceptingState(cis);
-//	  cis.close();
-//	}
+	@Test
+	public void UsagePatternTest26b() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
+	  KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+	  Assertions.extValue(0);
+	  keyGenerator.init(128);
+	  Assertions.extValue(0);
+	  SecretKey key = keyGenerator.generateKey();
+	  Assertions.hasEnsuredPredicate(key);
+	  Assertions.mustBeInAcceptingState(keyGenerator);
+	  Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+	  Assertions.extValue(0);
+	  cipher.init(Cipher.DECRYPT_MODE, key);
+	  Assertions.extValue(0);
+
+	  InputStream is = new FileInputStream(".\\resources\\cis.txt");
+	  CipherInputStream cis = new CipherInputStream(is, cipher);
+	  Assertions.extValue(0);
+	  Assertions.extValue(1);
+	  while (cis.read() != -1) {
+
+	  }
+	  Assertions.mustNotBeInAcceptingState(cis);
+	  cis.close();
+	}
 
 	@Test
 	public void UsagePatternTest26c() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
