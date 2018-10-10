413572d25cd2c0e94d3b22b158cbdfb699b9248a
Johannes Spaeth <joh.spaeth@googlemail.com>
2018-10-10 15:36:08 +0200

    Added missing import

--- CryptoAnalysis/src/test/java/tests/typestate/SecureRandomTest.java
+++ CryptoAnalysis/src/test/java/tests/typestate/SecureRandomTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import test.IDEALCrossingTestingFramework;
