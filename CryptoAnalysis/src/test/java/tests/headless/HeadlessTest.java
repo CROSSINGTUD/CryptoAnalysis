5ced56f0aa9e9ef79d8e22c977966600e566ce83
Johannes Späth <joh.spaeth@googlemail.com>
2018-11-20 15:41:58 +0100

    Cleanup & Rename of HeadlessTests -> HeadlessTest, as only then the test cases are executed

--- CryptoAnalysis/src/test/java/tests/headless/HeadlessTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/HeadlessTest.java
@@ -46,7 +46,7 @@ import sync.pds.solver.nodes.Node;
 import test.IDEALCrossingTestingFramework;
 import typestate.TransitionFunction;
 
-public class HeadlessTests {
+public class HeadlessTest {
 
 	/**
 	 * For running these test cases in Eclipse, specify your maven home path as JVM argument:
@@ -187,24 +187,23 @@ public class HeadlessTests {
 		scanner.exec();
 		assertErrors();
 	}
-//
-//	@Test
-//	public void stopwatchExample() {
-//		String applicationClassPath = new File("../CryptoAnalysisTargets/StopwatchExample/bin").getAbsolutePath();
-//		String sootClassPath = applicationClassPath + File.pathSeparator
-//				+ new File("../CryptoAnalysisTargets/StopwatchExample/guava-23.0.jar").getAbsolutePath();
-//		String rulesDir = new File("../CryptoAnalysisTargets/StopwatchExample/rules").getAbsolutePath();
-//		HeadlessCryptoScanner scanner = createAnalysisFor(applicationClassPath, sootClassPath, rulesDir);
-//		//TODO this is wrong. The state machine does not label the correct accepting states for the state machine.
-//		setErrorsCount("<main.Main: void correct()>", IncompleteOperationError.class, 2);
-//		setErrorsCount("<main.Main: void wrong()>", TypestateError.class, 1);
-//		setErrorsCount("<main.Main: void context(com.google.common.base.Stopwatch)>", TypestateError.class, 1);
-//		setErrorsCount("<main.Main: void context2(com.google.common.base.Stopwatch)>", TypestateError.class, 1);
-//		scanner.exec();
-//		assertErrors();
-//
-//	}
-//
+
+	@Test
+	public void loadExternalRuleAndStopwatchExample() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/StopwatchExample").getAbsolutePath();
+		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+		String rulesDir = new File("../CryptoAnalysisTargets/StopwatchExample/rules").getAbsolutePath();
+		HeadlessCryptoScanner scanner = createScanner(mavenProject, rulesDir);
+		//TODO this is wrong. The state machine does not label the correct accepting states for the state machine.
+		setErrorsCount("<main.Main: void correct()>", IncompleteOperationError.class, 2);
+		setErrorsCount("<main.Main: void wrong()>", TypestateError.class, 1);
+		setErrorsCount("<main.Main: void context(com.google.common.base.Stopwatch)>", TypestateError.class, 1);
+		setErrorsCount("<main.Main: void context2(com.google.common.base.Stopwatch)>", TypestateError.class, 1);
+		scanner.exec();
+		assertErrors();
+
+	}
+
 	@Test
 	public void cryptoMisuseExampleProject() {
 		String mavenProjectPath = new File("../CryptoAnalysisTargets/CryptoMisuseExamples").getAbsolutePath();
