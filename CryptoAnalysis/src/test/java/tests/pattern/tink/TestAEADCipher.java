3b8ab8b4e07bc52e24a6478e1df1fd6c5fadaedf
Rodrigo Bonifácio <rbonifacio123@gmail.com>
2018-11-21 08:52:58 +0100

    ISSUE-92 Fix the Google Tink specifications

--- CryptoAnalysis/src/test/java/tests/pattern/tink/TestAEADCipher.java
+++ CryptoAnalysis/src/test/java/tests/pattern/tink/TestAEADCipher.java
@@ -15,18 +15,9 @@ import test.assertions.Assertions;
 
 public class TestAEADCipher extends UsagePatternTestingFramework {
 	
-	private static final String plainText = "Just testing the encryption mode of AEAD"; 
-	private static final String aad = "cryptsl";
-
-	@Test
-	public void simpleTest() {
-		KeyTemplate kt = AeadKeyTemplates.createAesGcmKeyTemplate(16);
-		Assertions.hasEnsuredPredicate(kt);	
-	}
-	
 	@Test
 	public void generateNewAES128GCMKeySet() throws GeneralSecurityException {
-		KeyTemplate kt = AeadKeyTemplates.createAesGcmKeyTemplate(16);
+		KeyTemplate kt = AeadKeyTemplates.AES256_GCM;
 		KeysetHandle ksh = KeysetHandle.generateNew(kt);
 		Assertions.hasEnsuredPredicate(kt);
 		Assertions.mustBeInAcceptingState(kt);
@@ -62,49 +53,14 @@ public class TestAEADCipher extends UsagePatternTestingFramework {
 		KeyTemplate kt = AeadKeyTemplates.AES128_GCM;
 		KeysetHandle ksh = KeysetHandle.generateNew(kt);
 		
-//		Assertions.mustBeInAcceptingState(kt);
-//		Assertions.mustBeInAcceptingState(ksh);	
-//		
+		final String plainText = "Just testing the encryption mode of AEAD"; 
+		final String aad = "cryptsl";
+		
 		Aead aead = AeadFactory.getPrimitive(ksh);
 		byte[] out = aead.encrypt(plainText.getBytes(), aad.getBytes());
+		
 		Assertions.mustBeInAcceptingState(aead);
 		Assertions.hasEnsuredPredicate(out);
 		
    	}
-	
-	
-	@Test
-	public void encryptUsingAES128GCM_refactored() throws GeneralSecurityException {
-		//In these test scenario, I do not suggest to use the static field to create the object
-		//The following is not yet working
-		//KeyTemplate kt = AeadKeyTemplates.AES128_GCM;
-		KeyTemplate kt = AeadKeyTemplates.createAesGcmKeyTemplate(16);
-		//Check that the newly created object receives a predicate
-		Assertions.hasEnsuredPredicate(kt); //works
-		KeysetHandle ksh = KeysetHandle.generateNew(kt);
-		
-		//Check that also this object receives a predicate
-		Assertions.hasEnsuredPredicate(ksh); //works
-		Assertions.mustBeInAcceptingState(kt); //works
-		Assertions.mustBeInAcceptingState(ksh);	//works
-
-		//I also suggest to either use stack variables to specify test cases
-		final String plainText = "Just testing the encryption mode of AEAD"; 
-		final String aad = "cryptsl";
-		
-		//The analysis expects a rule for "Aead", which it could not find here!
-		Aead aead = AeadFactory.getPrimitive(ksh);
-		byte[] out = aead.encrypt(plainText.getBytes(), aad.getBytes());
-		Assertions.mustBeInAcceptingState(aead); //works
-		Assertions.hasEnsuredPredicate(out); //fails
-		
-   	}
-	
-	
-//	@Test
-//	public void generateKeySetHandleWithInvalidTemplate() throws GeneralSecurityException {
-//		KeysetHandle ksh = KeysetHandle.generateNew(null);
-//		Assertions.mustNotBeInAcceptingState(ksh);
-//	}
-	
 }
