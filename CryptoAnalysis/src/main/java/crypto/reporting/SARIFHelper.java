e8e9d348de7f2378c64f9b6ee4ef5fb0ab03df8f
Kummita Sriteja <sriteja.ku@gmail.com>
2018-12-06 15:08:56 +0100

    Intitial version of SARIF reporter
    Intitial version of SARIF reporter

---
+++ CryptoAnalysis/src/main/java/crypto/reporting/SARIFHelper.java
@@ -0,0 +1,87 @@
+package crypto.reporting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+@SuppressWarnings("unchecked")
+public class SARIFHelper {
+	
+	Map<String, String> rulesMap;
+	
+	public SARIFHelper() {
+		rulesMap = new HashMap<>();
+		initialize();
+	}
+	
+	private void initialize() {
+		//SARIFConfig
+		this.rulesMap.put(SARIFConfig.CONSTRAINT_ERROR_KEY, SARIFConfig.CONSTRAINT_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.FORBIDDEN_METHOD_ERROR_KEY, SARIFConfig.FORBIDDEN_METHOD_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.IMPRECISE_VALUE_EXTRACTION_ERROR_KEY, SARIFConfig.IMPRECISE_VALUE_EXTRACTION_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.INCOMPLETE_OPERATION_ERROR_KEY, SARIFConfig.INCOMPLETE_OPERATION_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.NEVER_TYPE_OF_ERROR_KEY, SARIFConfig.NEVER_TYPE_OF_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.REQUIRED_PREDICATE_ERROR_KEY, SARIFConfig.REQUIRED_PREDICATE_ERROR_VALUE);
+		this.rulesMap.put(SARIFConfig.TYPE_STATE_ERROR_KEY, SARIFConfig.TYPE_STATE_ERROR_VALUE);
+	}
+	
+	public JSONObject initialJson() {
+		JSONObject json = new JSONObject();
+		json.put(SARIFConfig.VERSION, SARIFConfig.VERSION_NUMBER);
+		json.put(SARIFConfig.RUNS_KEY, new JSONArray().add(getRuns()));
+		return json;
+	}
+	
+	private JSONObject getRuns() {
+		JSONObject jsonRuns = new JSONObject();
+		jsonRuns.put(SARIFConfig.TOOL_KEY, getToolInfo());
+		return jsonRuns;
+	}
+	
+	public String getFileName(String fileName) {
+		return fileName.replace(".", "/") + ".java";
+	}
+	
+	public JSONObject getToolInfo() {
+		JSONObject tool = new JSONObject();
+		tool.put(SARIFConfig.TOOL_FULL_NAME_KEY, SARIFConfig.TOOL_FULL_NAME_VALUE);
+		tool.put(SARIFConfig.TOOL_FULL_NAME_KEY, SARIFConfig.TOOL_FULL_NAME_VALUE);
+		tool.put(SARIFConfig.VERSION, SARIFConfig.VERSION_NUMBER);
+		tool.put(SARIFConfig.SEMANTIC_VERSION_KEY,SARIFConfig.SEMANTIC_VERSION_VALUE);
+		tool.put(SARIFConfig.LANGUAGE_KEY, SARIFConfig.LANGUAGE_VALUE);
+		return tool;
+	}
+	
+	public JSONObject getMessage(String text, String richText) {
+		JSONObject message = new JSONObject();
+		message.put(SARIFConfig.TEXT_KEY, text);
+		message.put(SARIFConfig.RICH_TEXT_KEY, richText);
+		return message;
+	}
+	
+	public JSONArray getLocations(String fileName, String methodName, int lineNumber) {
+		JSONArray locations = new JSONArray();
+		JSONObject location = new JSONObject();
+		
+		JSONObject startLine = new JSONObject();
+		startLine.put(SARIFConfig.START_LINE_KEY, lineNumber);
+		JSONObject uri = new JSONObject();
+		uri.put(SARIFConfig.URI_KEY, getFileName(fileName));
+		JSONObject physicalLocation = new JSONObject();
+		physicalLocation.put(SARIFConfig.FILE_LOCATION_KEY, uri);
+		physicalLocation.put(SARIFConfig.REGION_KEY, startLine);
+		
+		location.put(SARIFConfig.PHYSICAL_LOCATION_KEY, physicalLocation);
+		String fullyQualifiedLogicalName = fileName.replace(".", "::") + "::" + methodName;
+		location.put(SARIFConfig.FULLY_QUALIFIED_LOGICAL_NAME_KEY, fullyQualifiedLogicalName);
+		
+		locations.add(location);
+		return locations;
+	}
+	
+	public String getRuleDescription(String ruleId) {
+		return this.rulesMap.get(ruleId);
+	}
+}
---
+++ CryptoAnalysis/src/main/java/crypto/reporting/SARIFReporter.java
@@ -0,0 +1,119 @@
+package crypto.reporting;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import crypto.analysis.IAnalysisSeed;
+import crypto.analysis.errors.AbstractError;
+import crypto.rules.CryptSLRule;
+import soot.SootClass;
+import soot.SootMethod;
+
+@SuppressWarnings("unchecked")
+public class SARIFReporter extends ErrorMarkerListener {
+
+	private File outputFolder;
+//	private List<CryptSLRule> rules;
+	private Collection<IAnalysisSeed> objects = new HashSet<>();
+	private JSONObject files = new JSONObject(), resources = new JSONObject(), rules = new JSONObject();
+	private JSONArray results = new JSONArray();
+	private SARIFHelper sarifHelper;
+
+	public SARIFReporter(String string, List<CryptSLRule> rules) {
+		this.outputFolder = (string != null ? new File(string) : null);
+		this.sarifHelper = new SARIFHelper();
+//		this.rules = rules;
+	}
+	
+	private void addFile(String fileName) {
+		String filePath = this.sarifHelper.getFileName(fileName);
+		JSONObject mimeType = new JSONObject();
+		mimeType.put(SARIFConfig.MIME_TYPE_KEY, SARIFConfig.MIME_TYPE_VALUE);
+		this.files.put(filePath, mimeType);
+	}
+	
+	private String addRules(String errorType) {
+		String finalErrorType = errorType;
+		if (this.rules.containsKey(errorType)) {
+			finalErrorType = errorType.concat("-1");
+		}
+		JSONObject ruleInfo = new JSONObject();
+		JSONObject fullDescription = new JSONObject();
+		fullDescription.put(SARIFConfig.TEXT_KEY, this.sarifHelper.getRuleDescription(errorType));
+		ruleInfo.put(SARIFConfig.RULES_ID_KEY, errorType);
+		ruleInfo.put(SARIFConfig.FULL_DESCRIPTION_KEY, fullDescription);
+		this.rules.put(finalErrorType, ruleInfo);
+		return finalErrorType;
+	}
+	
+	private void addResults(String errorType, String fileName, String methodName, int lineNumber, String text, String richText) {
+		JSONObject result = new JSONObject();
+		String finalErrorType = addRules(errorType);
+		result.put(SARIFConfig.RULE_ID_KEY, finalErrorType);
+		result.put(SARIFConfig.MESSAGE_KEY, this.sarifHelper.getMessage(text, richText));
+		result.put(SARIFConfig.LOCATIONS_KEY, this.sarifHelper.getLocations(fileName, methodName, lineNumber));
+		this.results.add(result);
+	}
+	
+	private JSONObject makeSARIF() {
+		this.resources.put(SARIFConfig.RULES_KEY, this.rules);
+		JSONObject sarif = new JSONObject();
+		sarif.put(SARIFConfig.VERSION, SARIFConfig.SARIF_VERSION_NUMBER);
+		JSONArray runs = new JSONArray();
+		JSONObject run = new JSONObject();
+		run.put(SARIFConfig.TOOL_KEY, this.sarifHelper.getToolInfo());
+		run.put(SARIFConfig.FILES_KEY, this.files);
+		run.put(SARIFConfig.RESULTS_KEY, this.results);
+		run.put(SARIFConfig.RESOURCES_KEY, this.resources);
+		runs.add(run);
+		sarif.put(SARIFConfig.RUNS_KEY, runs);
+		return sarif;
+	}
+
+	@Override
+	public void discoveredSeed(IAnalysisSeed object) {
+		this.objects.add(object);
+	}
+	
+	@Override
+	public void afterAnalysis() {
+		for (SootClass c : this.errorMarkers.rowKeySet()) {
+			addFile(c.getName());
+			
+			for (Entry<SootMethod, Set<AbstractError>> e : this.errorMarkers.row(c).entrySet()) {
+				for (AbstractError marker : e.getValue()) {
+					String errorType = marker.getClass().getSimpleName();
+					String richText = String.format("%s violating CrySL rule for %s.", marker.getClass().getSimpleName() ,marker.getRule().getClassName());
+					String text = String.format("%s.", marker.toErrorMarkerString());
+					int lineNumber = marker.getErrorLocation().getUnit().get().getJavaSourceStartLineNumber();
+					this.addResults(errorType, c.getName(), e.getKey().getName(), lineNumber, text, richText);
+				}
+			}
+		}
+		JSONObject sarif = makeSARIF();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String strJson = gson.toJson(sarif);
+		System.out.println(sarif.toJSONString().replaceAll("\\\\", ""));
+		if (outputFolder != null) {
+			try {
+				FileWriter writer = new FileWriter(outputFolder +"/CogniCrypt-SARIF-Report.txt");
+				writer.write(sarif.toString());
+				writer.close();
+			} catch (IOException e) {
+				throw new RuntimeException("Could not write to file " + outputFolder);
+			}
+		}
+	}
+}
