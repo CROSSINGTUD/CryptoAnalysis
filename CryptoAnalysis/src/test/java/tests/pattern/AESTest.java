784611720fbbb62df3b34646f0488c51ef7e1f90
Rakshit Krishnappa Ravi <rakshitkrishnapparavi@gmail.com>
2019-01-16 19:32:56 +0100

    Modified some test cases

--- CryptoAnalysis/src/test/java/tests/pattern/AESTest.java
+++ CryptoAnalysis/src/test/java/tests/pattern/AESTest.java
@@ -21,103 +21,155 @@ import test.assertions.Assertions;
 
 public class AESTest extends UsagePatternTestingFramework{
 
-	private static final byte[] tData   = Hex.decode("AAFE47EE82411A2BF3F6752AE8D7831138F041560631B114F3F6752AE8D7831138F041560631B1145A01020304050607");
-	
-	
-	@Override
-	protected String getSootClassPath() {
-		// TODO Auto-generated method stub
-		String sootCp = super.getSootClassPath();
-		
-		sootCp += File.pathSeparator + "/Users/rakshitkr/.m2/repository/org/bouncycastle/bcprov-jdk15on/1.60/bcprov-jdk15on-1.60.jar";
-		
-		System.out.println(sootCp);
-		return sootCp; 
-	}
+	private static final byte[] tData   = Hex.decode("355F697E8B868B65B25A04E18D782AFA");
 
 	@Test
-	public void testNullCBC1() throws DataLengthException, IllegalStateException, InvalidCipherTextException {
+	public void testAESEngine1() throws DataLengthException, IllegalStateException, InvalidCipherTextException {
+
 		BlockCipher engine = new AESEngine();
 		BlockCipher mode = new CBCBlockCipher(engine);
 		BufferedBlockCipher cipher =  new BufferedBlockCipher(mode);
 		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
 		ParametersWithIV params = new ParametersWithIV(kp, new byte[16]);
+
 		cipher.init(true, params);
+
 		byte[] out = new byte[cipher.getOutputSize(tData.length)];
 
-        int len = cipher.processBytes(tData, 0, tData.length, out, 0);
+		int len = cipher.processBytes(tData, 0, tData.length, out, 0);
 
-        len += cipher.doFinal(out, len);
-        
-        Assertions.hasEnsuredPredicate(engine);
-        Assertions.hasEnsuredPredicate(mode);
-        Assertions.hasEnsuredPredicate(params);
-        Assertions.mustBeInAcceptingState(cipher);
+		len += cipher.doFinal(out, len);
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.hasEnsuredPredicate(mode);
+		Assertions.hasEnsuredPredicate(params);
+
+		Assertions.mustBeInAcceptingState(cipher);
 	}
 
 	@Test
-	public void testNullCBC2() {
-		BlockCipher engine = new AESEngine(); //BlockCipher or AESEngine?
+	public void testAESEngine2() {
+
+		BlockCipher engine = new AESEngine();
 		BlockCipher mode = new CBCBlockCipher(engine);
-		Assertions.extValue(0);
 		BufferedBlockCipher cipher =  new BufferedBlockCipher(mode);
 		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
 		ParametersWithIV params = new ParametersWithIV(kp, new byte[16]);
+
 		cipher.init(true, params);
+
 		byte[] out = new byte[cipher.getOutputSize(tData.length)];
 
-        int len = cipher.processBytes(tData, 0, tData.length, out, 0);
-        
+		int len = cipher.processBytes(tData, 0, tData.length, out, 0);
 
-        // no final method
-        
-        Assertions.hasEnsuredPredicate(engine);
-        Assertions.hasEnsuredPredicate(mode);
-        Assertions.hasEnsuredPredicate(params);
-        Assertions.mustNotBeInAcceptingState(cipher);
-	}
-	
-	@Test
-	public void testAESLightEngine1() throws Exception { //modified based on CipherTest
-		BlockCipher engine = new AESLightEngine();	//here the reference type is different from instantiation
-		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
-		byte[]      buf = new byte[128];
-		engine.init(true, kp);
-		engine.processBlock(buf, 0, buf, 0);
-		Assertions.mustBeInAcceptingState(engine);
-//		Assertions.hasEnsuredPredicate(buf);
-//		Assertions.hasEnsuredPredicate(engine);
-	}
-	
-	@Test
-	public void testAESLightEngine2() throws Exception { //modified based on BlockCipherVectorTest & BlockCipherMonteCarloTest
-		BlockCipher engine = new AESLightEngine();
-//		BlockCipher mode = new CBCBlockCipher(engine);
-		BufferedBlockCipher cipher =  new BufferedBlockCipher(engine);	//here the reference variable is same type as instantiation
-		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
-//		ParametersWithIV params = new ParametersWithIV(kp, new byte[16]);
-		cipher.init(true, kp);
-		byte[] out = new byte[cipher.getOutputSize(tData.length)];
+		// no final method
 
-        int len = cipher.processBytes(tData, 0, tData.length, out, 0);
-        cipher.doFinal(out, len);
-        Assertions.mustBeInAcceptingState(cipher);
-        Assertions.mustBeInAcceptingState(engine);
 		Assertions.hasEnsuredPredicate(engine);
-//		Assertions.hasEnsuredPredicate(mode);
-//		Assertions.hasEnsuredPredicate(out);
+		Assertions.hasEnsuredPredicate(mode);
+		Assertions.hasEnsuredPredicate(params);
+
+		Assertions.mustNotBeInAcceptingState(cipher);
 	}
-	
-	
-	// to be moved to different test class
+
 	@Test
-	public void testCipher() throws GeneralSecurityException {
-		Cipher c = Cipher.getInstance("AES/CBC/PKCS7Padding");
-		c.init(1, (KeyGenerator.getInstance("AES")).generateKey());
-		byte[] input = "jdsljflsd".getBytes();
-		c.update(input);
-		byte[] out = c.doFinal();
-		Assertions.mustBeInAcceptingState(c);
-//		Assertions.hasEnsuredPredicate(out);
+	public void testAESLightEngine1() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
+
+		engine.init(true, kp);
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.hasEnsuredPredicate(kp);
+
+		Assertions.mustBeInAcceptingState(engine);
+	}
+
+	@Test
+	public void testAESLightEngine2() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+
+		//no init
+
+		byte[] buf = new byte[128];
+
+		engine.processBlock(buf, 0, buf, 0);
+
+		Assertions.hasEnsuredPredicate(engine);
+
+		Assertions.mustBeInAcceptingState(engine);
+	}
+
+	@Test
+	public void testAESLightEngine3() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
+
+		engine.init(true, kp);
+
+		byte[] correctBuf = new byte[engine.getBlockSize()];
+		byte[] shortBuf = new byte[correctBuf.length / 2];
+
+		engine.processBlock(shortBuf, 0, correctBuf, 0);	//failed short input check
+		engine.processBlock(correctBuf, 0, shortBuf, 0);	//failed short output check
+		engine.processBlock(correctBuf, 0, correctBuf, 0);
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.hasEnsuredPredicate(kp);
+
+		Assertions.mustBeInAcceptingState(engine);		
+	}
+
+	@Test
+	public void testAESLightEngine4() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+		byte[] dudKey = new byte[6];	//failed key length check
+		KeyParameter kp = new KeyParameter(dudKey);
+		engine.init(true, kp);
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.hasEnsuredPredicate(kp);
+
+		Assertions.mustBeInAcceptingState(engine);
+	}
+
+	@Test
+	public void testAESLightEngine5() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+		byte[] iv = new byte[16];
+		ParametersWithIV kp = new ParametersWithIV(null, iv);	//failed parameter check
+		engine.init(true, kp);	
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.notHasEnsuredPredicate(kp);
+
+		Assertions.mustNotBeInAcceptingState(engine);
+	}
+
+
+	@Test
+	public void testMonteCarloAndVector() throws Exception {
+
+		BlockCipher engine = new AESLightEngine();
+		BufferedBlockCipher cipher =  new BufferedBlockCipher(engine);
+		KeyParameter kp = new KeyParameter(Hex.decode("5F060D3716B345C253F6749ABAC10917"));
+
+		cipher.init(true, kp);
+
+		byte[] out = new byte[cipher.getOutputSize(tData.length)];
+
+		int len = cipher.processBytes(tData, 0, tData.length, out, 0);
+
+		cipher.doFinal(out, len);
+
+		Assertions.hasEnsuredPredicate(engine);
+		Assertions.hasEnsuredPredicate(kp);
+
+		Assertions.mustBeInAcceptingState(engine);
+		Assertions.mustBeInAcceptingState(cipher);
 	}
 }
