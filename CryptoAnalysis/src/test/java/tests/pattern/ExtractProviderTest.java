99847679e30bd6f9f1c7bb001e535f25d88888a0
enriozuni <enriozuni@hotmail.com>
2019-01-30 10:45:59 +0100

    Added two new folders for CrySL rules to test if Provider Detection analysis chooses the right rules

--- CryptoAnalysis/src/test/java/tests/pattern/ExtractProviderTest.java
+++ CryptoAnalysis/src/test/java/tests/pattern/ExtractProviderTest.java
@@ -17,7 +17,7 @@ public class ExtractProviderTest extends UsagePatternTestingFramework{
 	public void UsagePatternTestExtractProvider1() throws GeneralSecurityException, UnsupportedEncodingException, FileNotFoundException, IOException {
 	  MessageDigest md = MessageDigest.getInstance("SHA-256", "BC");
 	//Added assertion for Provider Detection
-	  Assertions.extValue(0);
+	  Assertions.extValue(1);
 	}
 	
 }
