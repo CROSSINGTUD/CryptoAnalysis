37230d0b0961276e7e29a76c6e556bf7ccf0729e
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-06 00:06:30 +0200

    Headless tests for sample projects

---
+++ CryptoAnalysis/src/test/java/tests/headless/DummyHeadlessTest.java
@@ -0,0 +1,31 @@
+package tests.headless;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import crypto.HeadlessCryptoScanner;
+import test.IDEALCrossingTestingFramework;
+
+public class DummyHeadlessTest extends AbstractHeadlessTest {
+
+	@Test
+	public void testOne() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy9").getAbsolutePath();
+		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
+		
+		scanner.exec();
+	  	assertErrors();
+	}
+
+	@Test
+	public void testTwo() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy8").getAbsolutePath();
+		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
+		
+		scanner.exec();
+	  	assertErrors();
+	}
+}
