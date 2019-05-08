0d5f349e0954b9e999b14e9dbc82ac2460ae7b41
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-08 14:16:42 +0200

    Sample "java platform module system" project

---
+++ CryptoAnalysisTargets/JPMSExample/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author rakshitkr
+ *
+ */
+module org.demo.jpms_hello_world {
+	
+	requires java.base;
+	
+	exports org.demo.jpms;
+	
+}
---
+++ CryptoAnalysisTargets/JPMSExample/src/org/demo/jpms/HelloModularWorld.java
@@ -0,0 +1,11 @@
+package org.demo.jpms;
+
+public class HelloModularWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, modular World!");
+	}
+
+}
