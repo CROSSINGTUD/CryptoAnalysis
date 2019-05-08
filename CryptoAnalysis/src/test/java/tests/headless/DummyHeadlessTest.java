92ead2a30925ed6cbe2cf302b8eeba3d2d1bf855
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-08 14:19:14 +0200

    Added headless test case for JPMS

--- CryptoAnalysis/src/test/java/tests/headless/DummyHeadlessTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/DummyHeadlessTest.java
@@ -9,19 +9,29 @@ import test.IDEALCrossingTestingFramework;
 
 public class DummyHeadlessTest extends AbstractHeadlessTest {
 
-	@Test
-	public void testOne() {
-		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy9").getAbsolutePath();
-		MavenProject mavenProject = createAndCompile(mavenProjectPath);
-		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
-		
-		scanner.exec();
-	  	assertErrors();
-	}
+//	@Test
+//	public void testOne() {
+//		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy9").getAbsolutePath();
+//		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+//		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
+//		
+//		scanner.exec();
+//	  	assertErrors();
+//	}
 
+//	@Test
+//	public void testTwo() {
+//		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy8").getAbsolutePath();
+//		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+//		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
+//		
+//		scanner.exec();
+//	  	assertErrors();
+//	}
+	
 	@Test
-	public void testTwo() {
-		String mavenProjectPath = new File("../CryptoAnalysisTargets/Dummy8").getAbsolutePath();
+	public void testThree() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/JPMSExample").getAbsolutePath();
 		MavenProject mavenProject = createAndCompile(mavenProjectPath);
 		HeadlessCryptoScanner scanner = createScanner(mavenProject, IDEALCrossingTestingFramework.RESOURCE_PATH);
 		
