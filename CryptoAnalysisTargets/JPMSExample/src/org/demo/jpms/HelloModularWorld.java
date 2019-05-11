c5b86fd58ccd0c7f3672a45d1dd052b7e797992b
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-11 11:46:59 +0200

    Added cipher instance to main method

--- CryptoAnalysisTargets/JPMSExample/src/org/demo/jpms/HelloModularWorld.java
+++ CryptoAnalysisTargets/JPMSExample/src/org/demo/jpms/HelloModularWorld.java
@@ -1,11 +1,25 @@
 package org.demo.jpms;
 
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+
 public class HelloModularWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, modular World!");
+//		System.out.println("Hello, modular World!");
+		
+		try {
+			Cipher instance = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
 }
