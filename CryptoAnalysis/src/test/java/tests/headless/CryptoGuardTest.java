8d8bd1c720b5a3ec9d1a5da5b6964f7bd5447171
enriozuni <enriozuni@hotmail.com>
2019-06-14 12:23:51 +0200

    Added FN headless tests for brokenhash projects

--- CryptoAnalysis/src/test/java/tests/headless/CryptoGuardTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/CryptoGuardTest.java
@@ -62,27 +62,35 @@ public class CryptoGuardTest extends AbstractHeadlessTest {
 		assertErrors();
 	}
 	
-//	@Test
-//	public void brokenHashExamples() {
-//		String mavenProjectPath = new File("../CryptoAnalysisTargets/CryptoGuardExamples/brokenhash").getAbsolutePath();
-//		MavenProject mavenProject = createAndCompile(mavenProjectPath);
-//		HeadlessCryptoScanner scanner = createScanner(mavenProject);
-//		
-//		// This test case corresponds to the following project in CryptoGuard:
-//		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashABICase1.java
-//		setErrorsCount("<example.brokenhash.BrokenHashABICase1: void go(java.lang.String,java.lang.String)>", ConstraintError.class, 1);
-//		
-//		// ABICase2, -3, -4 not included as tests due to being similar to ABICase1 above
-//		// ABICase5, -6, -7, -8 not included as tests due to misuses not being caught by analysis
-//		// This test case corresponds to the following project in CryptoGuard:
-//		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashBBCase2.java
-//		setErrorsCount("<example.brokenhash.BrokenHashBBCase2: void main(java.lang.String[])>", ConstraintError.class, 1);
-//		
-//		// BBCase1, -3, -4 not included due to being similar to BBCase2 above
-//		// All other ABS cases not included due to misuses not being caught by analysis
-//		scanner.exec();
-//		assertErrors();
-//	}
+	@Test
+	public void brokenHashExamples() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/CryptoGuardExamples/brokenhash").getAbsolutePath();
+		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+		HeadlessCryptoScanner scanner = createScanner(mavenProject);
+		
+		// This test case corresponds to the following project in CryptoGuard:
+		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashABICase1.java
+		setErrorsCount("<example.brokenhash.BrokenHashABICase1: void go(java.lang.String,java.lang.String)>", ConstraintError.class, 1);
+		// ABICase2, -3, -4 not included as tests due to being similar to ABICase1 above
+		
+		// This test case corresponds to the following project in CryptoGuard:
+		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashABICase5.java
+		setErrorsCount(ConstraintError.class, new FalseNegatives(1, "ConstraintError not caught! //Related to https://github.com/CROSSINGTUD/CryptoAnalysis/issues/163"), "<example.brokenhash.BrokenHashABICase5: void go(java.lang.String)>");
+		// ABICase6, -7, -8 not included as tests due to being similar to ABICase5 above
+		
+		// This test case corresponds to the following project in CryptoGuard:
+		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashBBCase2.java
+		setErrorsCount("<example.brokenhash.BrokenHashBBCase2: void main(java.lang.String[])>", ConstraintError.class, 1);
+		// BBCase1, -3, -4 not included due to being similar to BBCase2 above
+		
+		// This test case corresponds to the following project in CryptoGuard:
+		// https://github.com/CryptoGuardOSS/cryptoapi-bench/blob/master/src/main/java/org/cryptoapi/bench/brokenhash/BrokenHashABSCase1.java
+		setErrorsCount(ConstraintError.class, new FalseNegatives(1, "ConstraintError not caught! //Related to https://github.com/CROSSINGTUD/CryptoAnalysis/issues/164"), "<example.brokenhash.BrokenHashABSCase1: void encrypt(java.lang.String, java.lang.String)>");		
+		// All other ABS cases not included due to being similar to the ABSCase1 above
+		
+		scanner.exec();
+		assertErrors();
+	}
 //	
 //	@Test
 //	public void pbeIterationExamples() {
