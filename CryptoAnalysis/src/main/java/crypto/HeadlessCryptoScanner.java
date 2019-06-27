b643455674ca61004d3614bde6ac826b26135f6d
Johannes Späth <joh.spaeth@googlemail.com>
2019-06-27 15:08:01 +0200

    Bugfix

--- CryptoAnalysis/src/main/java/crypto/HeadlessCryptoScanner.java
+++ CryptoAnalysis/src/main/java/crypto/HeadlessCryptoScanner.java
@@ -17,10 +17,8 @@ import org.apache.commons.cli.ParseException;
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
 
-import boomerang.callgraph.BoomerangICFG;
 import boomerang.callgraph.ObservableDynamicICFG;
 import boomerang.callgraph.ObservableICFG;
-import boomerang.callgraph.ObservableStaticICFG;
 import boomerang.debugger.Debugger;
 import boomerang.debugger.IDEVizDebugger;
 import boomerang.preanalysis.BoomerangPretransformer;
@@ -208,7 +206,7 @@ public abstract class HeadlessCryptoScanner {
 			protected void internalTransform(String phaseName, Map<String, String> options) {
 				BoomerangPretransformer.v().reset();
 				BoomerangPretransformer.v().apply();
-				ObservableStaticICFG observableDynamicICFG = new ObservableStaticICFG(new BoomerangICFG(false));
+				ObservableDynamicICFG observableDynamicICFG = new ObservableDynamicICFG(false);
 				List<CryptSLRule> rules = HeadlessCryptoScanner.this.getRules();
 				ErrorMarkerListener fileReporter;
 				if (sarifReport()) {
