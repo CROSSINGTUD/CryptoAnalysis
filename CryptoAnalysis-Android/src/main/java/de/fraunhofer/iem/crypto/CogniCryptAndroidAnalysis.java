f0159482884f379bc19f9ab908d1ca57cb9ddea9
AnakinSklavenwalker <anakin@gmx-topmail.de>
2019-11-13 11:00:38 +0100

    change wording

--- CryptoAnalysis-Android/src/main/java/de/fraunhofer/iem/crypto/CogniCryptAndroidAnalysis.java
+++ CryptoAnalysis-Android/src/main/java/de/fraunhofer/iem/crypto/CogniCryptAndroidAnalysis.java
@@ -212,7 +212,7 @@ public class CogniCryptAndroidAnalysis {
 		logger.info("Loaded " + rules.size() + " CrySL rules");
 		logger.info("Running CogniCrypt Analysis");
 		scanner.scan(rules);
-		logger.info("Terminated CogniCrypt Analysis");
+		logger.info("Finished CogniCrypt Analysis");
 		System.gc();
 		return errorListener.getErrors();
 	}
