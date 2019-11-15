08766bdf7d93ebdac876acadbaceeb23ec645980
Sebastian Leuer <sebastian.leuer@iem.fraunhofer.de>
2019-11-15 14:24:08 +0100

    added directory support

--- CryptoAnalysis/src/main/java/crypto/rules/CryptSLRuleReader.java
+++ CryptoAnalysis/src/main/java/crypto/rules/CryptSLRuleReader.java
@@ -2,6 +2,10 @@ package crypto.rules;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import crypto.cryptslhandler.CrySLModelReader;
 
@@ -33,4 +37,37 @@ public class CryptSLRuleReader {
 	public static CryptSLRule readFromSourceFile(File file) {
 		return getReader().readRule(file);
 	}
+
+    public static List<CryptSLRule> readFromDirectory(File directory) {
+        return readFromDirectory(directory, false);
+    }
+
+    public static List<CryptSLRule> readFromDirectory(File directory, boolean recursive) {
+        if (!directory.exists() || !directory.isDirectory())
+            return new ArrayList<>();
+
+        List<File> cryptSLFiles = new ArrayList<>();
+        findCryptSLFiles(directory, recursive, cryptSLFiles);
+        if (cryptSLFiles.size() == 0)
+            return new ArrayList<>();
+
+        CrySLModelReader reader = getReader();
+        List<CryptSLRule> rules = new ArrayList<>();
+        for (File file :cryptSLFiles)
+            rules.add(reader.readRule(file));
+
+        // TODO: Decide what happens with potential duplicates
+        return rules.stream().filter((x) -> x != null).collect(Collectors.toList());
+    }
+
+    private static void findCryptSLFiles(File directory, boolean recursive, Collection<File> resultCollection) {
+        for (File file: directory.listFiles())
+        {
+            if (file.isFile() && file.getName().endsWith(CrySLModelReader.cryslFileEnding))
+                resultCollection.add(file);
+
+            if (recursive && file.isDirectory())
+                findCryptSLFiles(file, recursive, resultCollection);
+        }
+    }
 }
