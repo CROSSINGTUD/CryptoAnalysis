1099a230633eddd7f6807e05d02efcc906c5de17
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-03-13 13:08:02 +0100

    initial commit

--- CryptoAnalysis/src/main/java/crypto/rules/CrySLRuleReader.java
+++ CryptoAnalysis/src/main/java/crypto/rules/CrySLRuleReader.java
@@ -1,22 +1,153 @@
 package crypto.rules;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import crypto.cryslhandler.CrySLModelReader;
 
 public class CrySLRuleReader {
 	private static CrySLModelReader csmr;
-	
-	public static CrySLRule readFromSourceFile(File file) {
-		if(csmr == null)
+
+	private static CrySLModelReader getReader(){
+		if (csmr == null)
+		{
 			try {
 				csmr = new CrySLModelReader();
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-		return csmr.readRule(file);
+			catch (MalformedURLException e){
+				e.printStackTrace();
+				// Sebastian:
+				// TODO: Current code could cause a NullPointerException
+				// Question: Is this Exception ever likely to happen?
+				// If no: Swallow it and 'accept' the NullPointerException
+				// If it can happen: Maybe we should re-throw a different exception (with this as the inner exception)
+				// Reason: In both cases (either NullPointerException or a custom) the creation of CryptSLRules
+				// is impossible if the MalformedURLException was thrown. So instead of allowing a generic NullPointerException
+				// we could throw something new that could be caught.
+			}
+		}
+		return csmr;
 	}
-	
+
+	public static CrySLRule readFromSourceFile(File file) {
+		return getReader().readRule(file);
+	}
+
+	public static List<CrySLRule> readFromDirectory(File directory) {
+		return readFromDirectory(directory, false);
+	}
+
+	public static List<CrySLRule> readFromDirectory(File directory, boolean recursive) {
+		if (!directory.exists() || !directory.isDirectory())
+			return new ArrayList<>();
+
+		List<File> cryptSLFiles = new ArrayList<>();
+		findCryptSLFiles(directory, recursive, cryptSLFiles);
+		if (cryptSLFiles.size() == 0)
+			return new ArrayList<>();
+
+		CrySLModelReader reader = getReader();
+		List<CrySLRule> rules = new ArrayList<>();
+		for (File file :cryptSLFiles)
+			rules.add(reader.readRule(file));
+
+		// TODO: Decide what happens with potential duplicates
+		return rules.stream().filter((x) -> x != null).collect(Collectors.toList());
+	}
+
+	// TODO: Discuss about .zip layout: Only Root or allow recursive.
+	// TODO: Discuss whether to throw IOExceptions or not
+	public static List<CrySLRule> readFromZipFile(File file) {
+		if (!file.exists() || !file.isFile() || !file.getName().endsWith(".zip"))
+			return new ArrayList<>();
+
+		List<CrySLRule> rules = new ArrayList<>();
+
+		try {
+			ZipFile zip = new ZipFile(file);
+			for (Enumeration e = zip.entries(); e.hasMoreElements(); ) {
+				ZipEntry entry = (ZipEntry) e.nextElement();
+				if (!entry.isDirectory()) {
+					CrySLRule rule = getCrySLRuleFromZipEntry(entry, zip, file);
+					rules.add(rule);
+				}
+			}
+		}
+		catch (IOException e) {
+			return new ArrayList<>();
+		}
+
+		// TODO: Decide what happens with potential duplicates
+		return rules.stream().filter((x) -> x != null).collect(Collectors.toList());
+	}
+
+	private static void findCryptSLFiles(File directory, boolean recursive, Collection<File> resultCollection) {
+		for (File file: directory.listFiles())
+		{
+			if (file.isFile() && file.getName().endsWith(CrySLModelReader.cryslFileEnding))
+				resultCollection.add(file);
+
+			if (recursive && file.isDirectory())
+				findCryptSLFiles(file, recursive, resultCollection);
+		}
+	}
+
+	private  static CrySLRule getCrySLRuleFromZipEntry(ZipEntry entry, ZipFile zip, File zipFile)
+	{
+		if (entry.isDirectory() || !entry.getName().endsWith(CrySLModelReader.cryslFileEnding))
+			return null;
+		try {
+			String name = createUniqueZipEntryName(zipFile, entry);
+			CrySLRule rule = getReader().readRule(zip.getInputStream(entry), name);
+			return rule;
+		}
+		catch (IOException ex) {
+			return null;
+		}
+	}
+
+	// For zip file entries there is no real URI. Using the raw absolute path of the zip file will cause a exception
+	// when trying to resolve/create the resource in the CrySLModelReader:readRule() methods.
+	// Solution: Create a custom URI with the following scheme:
+	// uri := [HexHashedAbsoluteZipFilePath][SystemFileSeparator][ZipEntryName]
+	// This scheme has the properties that it still is unique system-wide,
+	// The hash will be the same for the same file, so you could know if two rules come from the same ruleset file
+	// and you still can get the information of the zipped file.
+	private static String createUniqueZipEntryName(File zipFile, ZipEntry zipEntry) {
+		if (!zipFile.exists() || !zipFile.isFile() || zipEntry == null)
+			return null;
+		StringBuilder sb = new StringBuilder();
+
+		String partFileName;
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update(zipFile.getAbsolutePath().getBytes());
+			partFileName = bytesToHex(messageDigest.digest());
+		}
+		catch (NoSuchAlgorithmException e) {
+			partFileName = zipFile.getName();
+		}
+
+		sb.append(partFileName);
+		sb.append(File.separator);
+		sb.append(zipEntry.getName());
+		return sb.toString();
+	}
+
+	private static String bytesToHex(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (byte b : bytes)
+			sb.append(String.format("%02x", b));
+		return sb.toString();
+	}
 }
