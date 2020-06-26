dc0cd1a5b645932cd30b19a3a1051794fd18bad7
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-06-26 12:56:53 +0200

    - added test case

---
+++ CryptoAnalysis/src/test/java/tests/headless/bugfixes/NullpointerPredicateForFields.java
@@ -0,0 +1,32 @@
+package tests.headless.bugfixes;
+
+import java.io.File;
+
+import org.junit.Test;
+
+
+import crypto.HeadlessCryptoScanner;
+import crypto.analysis.errors.ConstraintError;
+import crypto.analysis.errors.IncompleteOperationError;
+import crypto.analysis.errors.RequiredPredicateError;
+import crypto.analysis.errors.TypestateError;
+import tests.headless.AbstractHeadlessTest;
+import tests.headless.MavenProject;
+
+/**
+ * Refers to https://github.com/CROSSINGTUD/CryptoAnalysis/issues/270
+ */
+public class NullpointerPredicateForFields extends AbstractHeadlessTest {
+	@Test
+	public void issue270() {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/Bugfixes/issue270").getAbsolutePath();
+		MavenProject mavenProject = createAndCompile(mavenProjectPath);
+		HeadlessCryptoScanner scanner = createScanner(mavenProject);
+
+		setErrorsCount("<example.Launcher: void <init>()>", ConstraintError.class, 1);
+
+
+		scanner.exec();
+		assertErrors();
+	}
+}
