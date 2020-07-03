3505685b67639deb3dac409bebac4901aa35a68e
seena-mathew <seenamathew078@gmail.com>
2020-07-03 21:27:42 +0200

    Updated ignore package test case
    Signed-off-by: seena-mathew <seenamathew078@gmail.com>

--- CryptoAnalysis/src/test/java/tests/headless/AbstractHeadlessTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/AbstractHeadlessTest.java
@@ -1,14 +1,11 @@
 package tests.headless;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +14,7 @@ import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Table;
 import com.google.common.collect.Table.Cell;
+
 import boomerang.BackwardQuery;
 import boomerang.Query;
 import boomerang.jimple.Statement;
@@ -31,7 +29,6 @@ import crypto.analysis.CrySLRulesetSelector.Ruleset;
 import crypto.analysis.EnsuredCrySLPredicate;
 import crypto.analysis.IAnalysisSeed;
 import crypto.analysis.errors.AbstractError;
-import crypto.analysis.errors.RequiredPredicateError;
 import crypto.exceptions.CryptoAnalysisException;
 import crypto.extractparameter.CallSiteWithParamIndex;
 import crypto.extractparameter.ExtractedValue;
@@ -59,15 +56,29 @@ public abstract class AbstractHeadlessTest {
 	private static boolean VISUALIZATION = false;
 	private CrySLAnalysisListener errorCountingAnalysisListener;
 	private Table<String, Class<?>, Integer> errorMarkerCountPerErrorTypeAndMethod = HashBasedTable.create();
+	
 	/**
 	 * List for storing the package names to be ignored
 	 */
-	private static List<String> ignorePackageList = Collections.<String>emptyList();
+	private static List<String> ignorePackages = Collections.<String>emptyList();
+	
 	/**
 	 * Flag to test the ignore package functionality
 	 */
 	private static boolean IGNORE_PACKAGE = false;
 	
+	public static void setIgnorePackages(List<String> ignorePackageList) {
+		ignorePackages = ignorePackageList;
+	}
+	
+	public static boolean isIGNORE_PACKAGE() {
+		return IGNORE_PACKAGE;
+	}
+
+	public static void setIGNORE_PACKAGE(boolean iGNORE_PACKAGE) {
+		IGNORE_PACKAGE = iGNORE_PACKAGE;
+	}
+
 	protected MavenProject createAndCompile(String mavenProjectPath) {
 		MavenProject mi = new MavenProject(mavenProjectPath);
 		mi.compile();
@@ -120,22 +131,7 @@ public abstract class AbstractHeadlessTest {
 			
 			@Override
 			protected List<String> getIgnoredPackages(){
-				if(IGNORE_PACKAGE == true) {
-					try {
-						
-						File ignorePackage = new File("../CryptoAnalysisTargets/IgnorePackages.txt");
-						if(ignorePackage.exists()) {
-							ignorePackageList = Files.readAllLines(Paths.get(ignorePackage.getAbsolutePath()), StandardCharsets.UTF_8);
-							}
-						else {
-							throw new CryptoAnalysisException("Unable to read file from path : " + ignorePackage.getAbsolutePath());
-						}	
-					} catch (IOException | CryptoAnalysisException e) {
-							LOGGER.error(e.getMessage());
-					}
-				
-				}
-				return ignorePackageList;
+				return ignorePackages;
 			}
 		};
 		return scanner;
@@ -155,7 +151,12 @@ public abstract class AbstractHeadlessTest {
 					currCount = 0;
 				}
 				Integer newCount = --currCount;
-				if(!ignorePackageList.stream().anyMatch((s -> errorClassName.startsWith(s)))) {
+				if(isIGNORE_PACKAGE()) {
+					if(!ignorePackages.stream().anyMatch((s -> errorClassName.startsWith(s)))) {
+						errorMarkerCountPerErrorTypeAndMethod.put(methodContainingError, error.getClass(), newCount);
+				}
+				}
+				else {
 					errorMarkerCountPerErrorTypeAndMethod.put(methodContainingError, error.getClass(), newCount);
 				}
 			}
--- CryptoAnalysis/src/test/java/tests/headless/IgnorePackageTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/IgnorePackageTest.java
@@ -1,33 +1,31 @@
 package tests.headless;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
 import crypto.HeadlessCryptoScanner;
-import crypto.analysis.errors.ConstraintError;
-import crypto.analysis.errors.IncompleteOperationError;
-import crypto.analysis.errors.RequiredPredicateError;
-import crypto.analysis.errors.TypestateError;
+import crypto.exceptions.CryptoAnalysisException;
 
 public class IgnorePackageTest extends AbstractHeadlessTest{
 
-	@Test
-	public void loadMessageDigestExample() {
-		String mavenProjectPath = new File("../CryptoAnalysisTargets/CogniCryptDemoExample").getAbsolutePath();
+	@Test (expected = CryptoAnalysisException.class)
+	public void ignorePackageExample() throws IOException {
+		String mavenProjectPath = new File("../CryptoAnalysisTargets/IgnorePackageExample").getAbsolutePath();
 		MavenProject mavenProject = createAndCompile(mavenProjectPath);
 		HeadlessCryptoScanner scanner = createScanner(mavenProject);
-		
-		//false positive
-		setErrorsCount("<example.ConstraintErrorExample: void main(java.lang.String[])>", IncompleteOperationError.class, 1);
-		setErrorsCount("<example.ConstraintErrorExample: void main(java.lang.String[])>", ConstraintError.class, 1);
-		setErrorsCount("<example.IncompleOperationErrorExample: void main(java.lang.String[])>", IncompleteOperationError.class, 1);
-		setErrorsCount("<example.PredicateMissingExample: void main(java.lang.String[])>", ConstraintError.class, 1);
-		setErrorsCount("<example.PredicateMissingExample: void main(java.lang.String[])>", RequiredPredicateError.class, 1);
-		setErrorsCount("<example.TypestateErrorExample: void main(java.lang.String[])>", TypestateError.class, 1);
-		setErrorsCount("<example.fixed.ConstraintErrorExample: void main(java.lang.String[])>", IncompleteOperationError.class, 1);
+		setIGNORE_PACKAGE(true);
+		File ignorePackage = new File("../CryptoAnalysisTargets/IgnorePackages.txt");
+		setIgnorePackages(Files.readAllLines(Paths.get(ignorePackage.getAbsolutePath()), StandardCharsets.UTF_8));
 		scanner.exec();
-		assertErrors();
+		/*
+		* Ignored the 2 IncompleteOperationError in MessageDigestExample.MessageDigestExample.Main class as it specified in 
+		* IgnorePackages.txt
+		*/
 	}
 
 }
