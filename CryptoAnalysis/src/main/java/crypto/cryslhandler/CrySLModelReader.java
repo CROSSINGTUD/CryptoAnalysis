cd5ac73df5d3d39430719c1e74da7940f903c0a6
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-09-15 13:44:52 +0200

    update critical/missing dependencies

--- CryptoAnalysis/src/main/java/crypto/cryslhandler/CrySLModelReader.java
+++ CryptoAnalysis/src/main/java/crypto/cryslhandler/CrySLModelReader.java
@@ -613,8 +613,8 @@ public class CrySLModelReader {
 	}
 	private CrySLPredicate extractReqPred(final ReqPred pred) {
 		final List<ICrySLPredicateParameter> variables = new ArrayList<>();
-		PredLit innerPred = (PredLit) pred;			
-		final Constraint conditional = innerPred.getCons();
+		PredLit innerPred = (PredLit) pred;
+		final Constraint conditional = (Constraint) innerPred.getCons();
 		if (innerPred.getPred().getParList() != null) {
 			for (final SuPar var : innerPred.getPred().getParList().getParameters()) {
 				if (var.getVal() != null) {
