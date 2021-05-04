92dc98277894576fbcf50e95c5d9c8377d4788fd
Enri Ozuni <enriozuni@hotmail.com>
2021-05-04 16:06:10 +0200

    Fix failing build of develop branch in Jenkins CI (#344)
    * Downgrade maven invoker plugin to 3.2.1
    
    The version of the dependency was downgraded because it introduces errors to the test cases since there are API changes to this dependency
    
    * Fix NP exception in CSVReportCreationTest
    
    * Delete generated artifacts after ReportFormatTest completes

--- CryptoAnalysis/src/test/java/tests/headless/ReportFormatTest.java
+++ CryptoAnalysis/src/test/java/tests/headless/ReportFormatTest.java
@@ -1,6 +1,10 @@
 package tests.headless;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import crypto.HeadlessCryptoScanner;
@@ -8,9 +12,11 @@ import crypto.analysis.CryptoScannerSettings.ReportFormat;
 
 public class ReportFormatTest extends AbstractHeadlessTest{
 
-	private static final String txtReportPath = "cognicrypt-output/CryptoAnalysis-Report.txt";
-	private static final String csvReportPath = "cognicrypt-output/CryptoAnalysis-Report.csv";
-	private static final String sarifReportPath = "cognicrypt-output/CryptoAnalysis-Report.json";
+	private static final String rootPath = "cognicrypt-output/";
+	private static final String txtReportPath = rootPath+"CryptoAnalysis-Report.txt";
+	private static final String csvReportPath = rootPath+"CryptoAnalysis-Report.csv";
+	private static final String sarifReportPath = rootPath+"CryptoAnalysis-Report.json";
+	
 	@Test
 	public void TXTReportCreationTest() {
 		File report = new File(txtReportPath);
@@ -54,6 +60,15 @@ public class ReportFormatTest extends AbstractHeadlessTest{
 		HeadlessCryptoScanner scanner = createScanner(mavenProject);
 		scanner.exec();
 		Assert.assertTrue(report.exists());
+	}
+	
+	@After
+	public void tearDown() {
+		try {
+			FileUtils.deleteDirectory(new File(rootPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
