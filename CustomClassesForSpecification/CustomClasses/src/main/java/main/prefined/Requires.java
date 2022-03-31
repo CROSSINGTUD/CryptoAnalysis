f4e63ac5d977f3c3c8dbe027bb37858bb4d7ee7f
Marvin Vogel <91740473+marvinvo@users.noreply.github.com>
2022-03-31 17:18:20 +0200

    Add implicate

--- CustomClassesForSpecification/CustomClasses/src/main/java/main/prefined/Requires.java
+++ CustomClassesForSpecification/CustomClasses/src/main/java/main/prefined/Requires.java
@@ -49,4 +49,19 @@ public class Requires {
 	public void notPred1onP1_OR_pred1onP2_OR_notPred1onP3(A p1, A p2, A p3) {}
 	public void notPred1onP1_OR_notPred1onP2_OR_notPred1onP3(A p1, A p2, A p3) {}
 	
+	// IMPLICATE
+	
+	// same predicate
+	public void pred1onP1_IMPL_pred1onP2(A p1, A p2) {}
+	public void pred1onP1_IMPL_notPred1onP2(A p1, A p2) {}
+	
+	// multi predicates
+	public void pred1onP1_IMPL_pred2onP2(A p1, A p2) {}
+	public void pred1onP1_IMPL_notPred2onP2(A p1, A p2) {}
+	
+	// OR WITH IMPLICATION
+	// same predicate
+	public void pred1onP1_IMPL_pred1onP2_OR_pred1onP1_IMPL_pred2onP2(A p1, A p2) {}
+	public void pred1onP1_IMPL_pred1onP2_OR_pred1onP1_IMPL_!pred2onP2(A p1, A p2) {}
+	
 }
