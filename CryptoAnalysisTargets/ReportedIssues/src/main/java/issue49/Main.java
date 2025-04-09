32ddd81a4be11e3ff9b4ad736f45411aabf3ed8c
akshaya <akshayapuradam@gmail.com>
2025-04-10 00:03:37 +0200

    changes in main from Sha256withRSA to Kyber and dilithium

--- CryptoAnalysisTargets/ReportedIssues/src/main/java/issue49/Main.java
+++ CryptoAnalysisTargets/ReportedIssues/src/main/java/issue49/Main.java
@@ -5,13 +5,28 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Signature;
+import org.bouncycastle.pqc.jcajce.spec.KyberParameterSpec;
+import org.bouncycastle.pqc.jcajce.spec.DilithiumParameterSpec;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 	public byte[] sign(String data) throws Exception {
-		Signature signature = Signature.getInstance("SHA256withRSA");
-		signature.initSign(getPrivateKey());
-		signature.update(data.getBytes());
-		return signature.sign();
+
+		Security.addProvider(new BouncyCastleProvider());
+
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("Kyber", "BC");
+		kpg.initialize(KyberParameterSpec.kyber768, new SecureRandom());
+		KeyPair kyberKeyPair = kpg.generateKeyPair();
+
+		KeyPairGenerator sigGen = KeyPairGenerator.getInstance("Dilithium", "BC");
+		sigGen.initialize(DilithiumParameterSpec.dilithium3, new SecureRandom());
+		KeyPair dilithiumKeyPair = sigGen.generateKeyPair();
+
+		Signature pqSig = Signature.getInstance("Dilithium", "BC");
+		pqSig.initSign(dilithiumKeyPair.getPrivate());
+		pqSig.update("message".getBytes());
+
+		return pqSig.sign();
 	}
 
 	private PrivateKey getPrivateKey() throws NoSuchAlgorithmException {
